d57a7d36b301070eb8002d7fdb5faaf925b7346f
danad1821 <danadabdoub@gmail.com>
2025-11-11 17:36:29 +0200

    first commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+const WeatherAPI = " http://api.weatherapi.com/v1/bced4855a7614809aa4153324251111";
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
